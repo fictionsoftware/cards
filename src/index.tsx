8b44ca99e5b27c60fb7aa5b5e7d82c796b46121d
jasonleehodges <jasonleehodges@outlook.com>
2020-04-20 22:07:02 -0600

    fix: export from base

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,6 @@ export { CardCarousel } from './components/card-carousel';
 export { PaginationDots } from './components/pagination-dots';
 export { Flippable } from './components/flippable';
 export { Card } from './components/Card';
+export { Action } from './components/action';
+export { ActionDetails } from './components/action-details';
+export { Deck } from './components/Deck';
